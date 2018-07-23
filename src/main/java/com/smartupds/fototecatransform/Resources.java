8019e0e7b414ef82a8da377ed10279c0d4a5ee03
YannisMarketakis <ymark_em@yahoo.gr>
2018-07-23 12:26:27 +0300

    actual implementation

---
+++ src/main/java/com/smartupds/fototecatransform/Resources.java
@@ -0,0 +1,15 @@
+package com.smartupds.fototecatransform;
+
+/** Various common resources
+ * 
+ * @author Yannis Marketakis
+ * @author Nikos Minadakis 
+ */
+public class Resources {
+    public static final String BEANS_FILE="beans.xml";
+    public static final String X3ML_MAPPINGS_BEAN="x3ml_mappings_bean";
+    public static final String GENERATOR_POLICY_BEAN="generator_policy_bean";
+    public static final String UUID_SIZE_BEAN="uuid_size_bean";
+    public static final String SOURCE_FILES_BEAN="source_files_bean";
+    public static final String TRANSFORMED_FILES_BEAN="transformed_files_bean";
+}
--- src/main/java/com/smartupds/fototecatransform/TransformData.java
+++ src/main/java/com/smartupds/fototecatransform/TransformData.java
@@ -3,6 +3,8 @@ package com.smartupds.fototecatransform;
 import eu.delving.x3ml.X3MLEngineFactory;
 import java.io.File;
 import lombok.extern.log4j.Log4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /** A Transformer class that uses X3ML Engine for transforming Fototeca XML resources
  * 
@@ -14,24 +16,18 @@ public class TransformData {
     private final File x3mlMappings;
     private final File generatorPolicy;
     private final int uuidSize;
-    private final X3MLEngineFactory.OutputFormat outputFormat;
     
-    /** Creates a new instance and defines the default resources for transforming data (i.e. X3ML mappings, generator policy, 
-     * UUID size and output format). 
+    /** Creates a new instance and defines the default resources for transforming data (i.e. X3ML mappings, generator policy 
+     *  and UUID size). It is initialized using Spring. 
      * 
-     * @param x3mlMapppingsFile the file containing the X3ML mappings 
-     * @param generatorPolicyFile the file containing the generator policy definitions
-     * @param uuidSize the size of the generated UUIDs (use negative number for default UUID size)
-     * @param outputFormat the format of the transformed data */
-    public TransformData(File x3mlMapppingsFile, File generatorPolicyFile, int uuidSize, X3MLEngineFactory.OutputFormat outputFormat){
-        this.x3mlMappings=x3mlMapppingsFile;
+     * @param context The application context as defined from Spring */
+    public TransformData(ApplicationContext context){
+        this.x3mlMappings=context.getBean(Resources.X3ML_MAPPINGS_BEAN,File.class);
         log.info("X3ML Mappings file path: "+this.x3mlMappings);
-        this.generatorPolicy=generatorPolicyFile;
+        this.generatorPolicy=context.getBean(Resources.GENERATOR_POLICY_BEAN,File.class);
         log.info("Generator Policy file path: "+this.generatorPolicy);
-        this.uuidSize=uuidSize;
+        this.uuidSize=context.getBean(Resources.UUID_SIZE_BEAN,int.class);
         log.info("UUID Size: "+this.uuidSize);
-        this.outputFormat=outputFormat;
-        log.info("Output format: "+this.outputFormat);
     }
     
     /** Transforms the contents of the source file and exports the contents in the target file. 
@@ -41,10 +37,33 @@ public class TransformData {
      * @param from the file containing resources in the original format
      * @param to the file were the transformed contents will be exported */
     private void transformResource(File from, File to){
-        
+        log.info("Transforming file "+from+" to "+to);
+        X3MLEngineFactory.create()
+                         .withMappings(this.x3mlMappings)
+                         .withGeneratorPolicy(this.generatorPolicy)
+                         .withUuidSize(this.uuidSize)
+                         .withInputFiles(from)
+                         .withOutput(to, X3MLEngineFactory.OutputFormat.RDF_XML)
+                         .execute();
+    }
+    
+    /** Transforms all the XML resources from the source folder and exports the transformed 
+     * contents to the destination folder. For each resource in the source folder it creates a file in the 
+     * destination folder with the same name (and a different extension). 
+     * 
+     * @param fromFolder the folder containing the source files
+     * @param toFolder the folder containing the transformed files */
+    public void transformAllResources(File fromFolder, File toFolder){
+        log.info("Transforming sources from folder "+fromFolder+" to folder "+toFolder);
+        for(File sourceFile : fromFolder.listFiles()){
+            transformResource(sourceFile, new File(toFolder+"/"+sourceFile.getName().replace(".xml", ".rdf")));
+        }
     }
     
     public static void main(String[] args){
-        
+        ApplicationContext context=new ClassPathXmlApplicationContext(Resources.BEANS_FILE);
+        TransformData transformData=new TransformData(context);
+        transformData.transformAllResources(context.getBean(Resources.SOURCE_FILES_BEAN, File.class),
+                                            context.getBean(Resources.TRANSFORMED_FILES_BEAN, File.class));
     }
 }
